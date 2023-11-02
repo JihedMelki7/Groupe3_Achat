304727640f586c42d2004739dfb873ded5fa4ca0
bouaradanourchene <nourchene.bouarada@esprit.tn>
2023-11-02 19:25:22 +0100

    coriger

--- src/test/java/tn/esprit/rh/achat/StockTest.java
+++ src/test/java/tn/esprit/rh/achat/StockTest.java
@@ -6,19 +6,18 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.context.ContextConfiguration;
 import org.testng.annotations.Test;
 import tn.esprit.rh.achat.entities.Stock;
 import tn.esprit.rh.achat.repositories.StockRepository;
 import tn.esprit.rh.achat.services.StockServiceImpl;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 @RunWith(MockitoJUnitRunner.class)
-
+@ContextConfiguration(classes = {StockServiceImpl.class})
 public class StockTest {
 
     @Mock

75c5357002e32b83314b367b19691af3a89cbde9
bouaradanourchene <nourchene.bouarada@esprit.tn>
2023-11-02 16:45:56 +0100

    test

---
+++ src/test/java/tn/esprit/rh/achat/StockTest.java
@@ -0,0 +1,47 @@
+package tn.esprit.rh.achat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.Test;
+import tn.esprit.rh.achat.entities.Stock;
+import tn.esprit.rh.achat.repositories.StockRepository;
+import tn.esprit.rh.achat.services.StockServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+
+public class StockTest {
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @InjectMocks
+    private StockServiceImpl stockService;
+    @Mock
+    private StockRepository stockRepository;
+
+    @Test
+    void testRetrieveAllStocks() {
+        List<Stock> mockStocks = new ArrayList<>();
+        mockStocks.add(new Stock());
+        Mockito.when(stockRepository.findAll()).thenReturn(mockStocks);
+        List<Stock> result = stockService.retrieveAllStocks();
+        assertEquals(mockStocks, result);
+    }
+
+    @Test
+    void testDeleteStock() {
+        stockService.deleteStock(1L);
+        verify(stockRepository).deleteById(eq(1L));
+    }
+
+
+}
